c102199e7bd38cf864145cd981a1782688dfbfcd
dahaitec <fuureibou@gamil.com>
2019-09-07 23:21:25 +0900

    l

---
+++ test/test/Classes/Main/View/PageTitleView.swift
@@ -0,0 +1,21 @@
+//
+//  PageTitleView.swift
+//  test
+//
+//  Created by Sheng Zhao Huang on 2019/9/7.
+//  Copyright © 2019 Sheng Zhao Huang. All rights reserved.
+//
+
+import UIKit
+
+class PageTitleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
